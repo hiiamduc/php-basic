09e8dfe310aeda7f9691d87c0cc2500d32bce89b
hiiamduc <hiiamduc@gmail.com>
2016-09-13 19:23:24 +0700

    array
    The array supports

---
+++ array_function.php
@@ -0,0 +1,31 @@
+function array_search_recursive($value,$array)
+{
+    if(is_array($value))
+    {
+        $length = count($value);
+        $return = array();
+        var_dump($value);
+        foreach($value as $val1)
+        {
+            if (count($return) == $length) break;
+            foreach($array as $key => $val2)
+            {
+                if($val1 == $val2)
+                {
+                    $return[] = $key;
+                    break;
+                }
+            } 
+        } 
+            return !empty($return) ? $return : false;
+    } else {
+        foreach($array as $key => $val)
+        {
+            if($value == $val or (is_array($value) && array_search_recursive($value,$array)!=false))
+            {
+                return $key;
+            }
+        }
+        return false;
+    }
+}
